2a051bc8ba4cabc3de75062c155cf281a643a464
Cinthya Lins <cml2@cin.ufpe.br>
2021-01-13 22:07:04 -0300

    operadores basicos

---
+++ Aula003_13012021/aula003_2.py
@@ -0,0 +1,11 @@
+# Operadores Básicos
+a = 10
+b = 20
+
+print(f'soma:{a+b}') #soma
+print(f'subtração: {a-b}') #subtração
+print(f'multiplicação: {a*b}') #multiplicação
+print(f'divisão: {a/b}') #divisão
+print(f'módulo: {a%b}') #módulo ou resto
+print(f'potenciação: {a**b}') #potenciação
+print(f'divisão float: {float(a)//float(b)}') #divisão float
