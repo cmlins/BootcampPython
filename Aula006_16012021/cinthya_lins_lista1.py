20fe6de690fe6f414774a049854afe62d53a9a62
Cinthya Lins <cml2@cin.ufpe.br>
2021-01-17 19:24:23 -0300

    exercicio 14

--- Aula006_16012021/cinthya_lins_lista1.py
+++ Aula006_16012021/cinthya_lins_lista1.py
@@ -224,6 +224,34 @@ print('\n')
 # 14. Fazer um sistema de biblioteca (Deve imprimir uma lista com 10 livros, pedir o nome do solicitante do empréstimo, pedir para selecionar um livro e imprimir o livro selecionado)
 print('--- Exercício 14 ---\n')
 
+livros = {
+    1 : " Iluminadas por Lauren Beukes",
+    2 : " Comer, rezar, amar por Elizabeth Gilbert",
+    3 : " Outros Jeitos de Usar a Boca por Rupi Kaur", 
+    4 : " Meu corpo minha casa  por Rupi Kaur",
+    5 : " Tudo nela brilha e queima por Ryane Leão",
+    6 : " Pequeno manual antirracista por Djamila Ribeiro",
+    7 : " Sejamos todos feministas por Chimamanda Ngozi Adichie",
+    8 : " Labirinto por Kate Mosse",
+    9 : " Cracking the coding interview por Gayle Laakmann McDowell",
+    10 : " O momento de voar por Melinda Gates"
+}
+
+print("Os livros disponíveis são:\n")
+
+for opcao in livros:
+    print(f'{opcao} - {livros[opcao]}')
+
+nome = input("\nInsira seu nome: ")
+
+livro = int(input("Digite o número do livro que escolheu: "))
+
+if (livro in range(len(livros) + 1)):
+    print(f"{nome}, você escolheu o livro{livros[livro]}!")
+else: 
+    print("Você não escolheu uma opção válida")
+
+print('\n')
 # 15. Fazer um sistema de Feira Livre(Deve imprimir uma lista com as frutas e pedir para o solicitante colocar o nome e selecionar a fruta e depois deve imprimir o nome do solicitante e a fruta)
 print('--- Exercício 15 ---\n')
 
--- Aula006_16012021/testes.py
+++ Aula006_16012021/testes.py
@@ -1,16 +1,35 @@
-nome = input('Insira seu nome: ')
+# 14. Fazer um sistema de biblioteca (Deve 
+# imprimir uma lista com 10 livros, 
+# pedir o nome do solicitante do empréstimo, 
+# pedir para selecionar um livro e 
+# imprimir o livro selecionado)
 
-print('\nDigite o número correspondente ao seu destino:\n1 - Paris;\n 2 - Londres;\n 3 - Bruxelas;\n 4 - Amsterdam')
+livros = {
+    1 : " Iluminadas por Lauren Beukes",
+    2 : " Comer, rezar, amar por Elizabeth Gilbert",
+    3 : " Outros Jeitos de Usar a Boca por Rupi Kaur", 
+    4 : " Meu corpo minha casa  por Rupi Kaur",
+    5 : " Tudo nela brilha e queima por Ryane Leão",
+    6 : " Pequeno manual antirracista por Djamila Ribeiro",
+    7 : " Sejamos todos feministas por Chimamanda Ngozi Adichie",
+    8 : " Labirinto por Kate Mosse",
+    9 : " Cracking the coding interview por Gayle Laakmann McDowell",
+    10 : " O momento de voar por Melinda Gates"
+}
 
-destino = int(input())
+print("Os livros disponíveis são:\n")
 
-if (destino == 1):
-    print(f'{nome}, você escolheu visitar Paris!')
-elif (destino == 2):
-    print(f'{nome}, você escolheu visitar Londres!')
-elif (destino == 3):
-    print(f'{nome}, você escolheu visitar Bruxelas!')
-elif (destino == 4):
-    print(f'{nome}, você escolheu visitar Amsterdam!')
+for opcao in livros:
+    print(f'{opcao} - {livros[opcao]}')
+
+nome = input("\nInsira seu nome: ")
+
+livro = int(input("Digite o número do livro que escolheu: "))
+
+# print(f"{nome}, você escolheu o livro{livros[livro]}!")
+# print(range(len(livros)))
+
+if (livro in range(len(livros) + 1)):
+    print(f"{nome}, você escolheu o livro{livros[livro]}!")
 else: 
-    print('Você não escolheu uma opção válida')
+    print("Você não escolheu uma opção válida")
